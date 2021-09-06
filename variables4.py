641fc715226bbdc4e1cccd5b255cffcbc3cad367
Hasan Çelik <24228975+Hasannccelik@users.noreply.github.com>
2021-09-07 00:00:18 +0300

    Add files via upload
    3 sayısını 3 farklı değişken tipi ile tanımladık x string yani kelime harf olarak y int yani sayı olarak z float yani virgüllü sayı olarak tanımladık.

---
+++ variables4.py
@@ -0,0 +1,6 @@
+x=str(3) # x burada string olarak 3 sayısı aritmetik işlem yapılamaz
+y=int(3)# y burada ise int olarak tanımlandı aritmetik işlem yapabiliriz.
+z=float(3)# z burada float olarak tanımlandı. 3.000 olarak aritmetik işlem yapabiliriz.
+print(x)
+print(y)
+print(z)
