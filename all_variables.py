3f9d19d673b73187e67a89631b9c1cc2cc1ffc9e
Hasan Çelik <fdaclock.159@gmail.com>
2021-09-09 23:58:10 +0300

    Create all_variables.py
    burada ise Python dilindeki tüm değişken tiplerini tanımladık ancak tüm değişkenlerin değerlerini gösteremedim tek tek yazmam gerekirdi şimdilik böyle dursun

---
+++ all_variables.py
@@ -0,0 +1,20 @@
+
+def my func():
+
+a="Hello Word" #string olarak tanımlanan değişken
+b=20 #int değerinde tanımlanan değişken
+c=20.5 #float olarak tanımlanan değişken
+d= 1j #complex olarak tanımlanan değişken
+e=["apple","banana","cherry"] #list olarak tanımlanan değişken
+f=("apple","banana","cherry") #tuple olarak tanımlanan değişken
+g= range(6) #range olarak tanımlanan değişken
+h={"fruit":"banana","taste":"sweet"}# dictionary(sözlük) olarak tanımlanan değişken
+ı={"apple","banana","cherry"} #set olarak tanımlanan değişken
+i=frozenset({"apple","banana","cherry"}) #frozenset olarak tanımlanan değişken
+j=True #bool olarak tanımlanan değişken
+k=b"Hello" #byte olarak tanımlanan değişken
+l=bytearray(5) #bytearray olarak tanımlanan değişken
+m=memoryview(bytes(5)) #memoryview olarak tanımlanan değişken
+
+
+myfunc()
