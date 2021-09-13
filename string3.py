023044fc4e9ebb7595ba2b3b7445c70a5ce18ac8
Hasan Çelik <fdaclock.159@gmail.com>
2021-09-13 23:10:01 +0300

    Create string3.py
    string i yorum satırına benzer bir şekilde üç çift tırnak kullanarak ve üç tek tırnak kullanarak kullanabiliriz.

---
+++ string3.py
@@ -0,0 +1,7 @@
+a ='''Elit nostrud culpa est nisi nostrud veniam quis sit eu magna consectetur eiusmod non non.
+ Lorem pariatur laboris consectetur enim. Laborum ut ipsum eu dolore fugiat nisi mollit et proident quis nisi aliqua quis ad.
+  Velit ea laborum dolore irure ex id officia. '''
+print(a)
+
+b=""" Officia laborum ex nulla amet eu velit ea elit in et fugiat anim. Enim anim enim do et nisi veniam do cillum est ex aliqua eu proident. Incididunt deserunt consequat aliquip aliqua voluptate consectetur pariatur pariatur voluptate in minim et Lorem. Irure commodo cupidatat amet irure cupidatat amet voluptate. Ad magna qui magna aliquip est id tempor. Proident adipisicing in ullamco labore tempor deserunt excepteur officia sint duis."""
+print(b)
